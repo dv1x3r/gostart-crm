6653944ac2f71c5872386aa4ba0783c75d2ffa65
dv1x3r <d.purvinsh@gmail.com>
2023-11-01 17:38:36 +0200

    add logging

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"text/template"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data any, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	e := echo.New()
+
+	e.Debug = true
+	e.Renderer = &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+
+	e.Use(middleware.Logger())
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		// return c.String(http.StatusOK, "Hello, World!")
+		// c.Logger().Debug("yes")
+		return c.Render(http.StatusOK, "hello", "World")
 	})
 	e.GET("/users/:id", getUser)
 	e.GET("/show", show)
