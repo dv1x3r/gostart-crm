44975adf89d75b724adaae767b905499b75a38ea
dv1x3r <d.purvinsh@gmail.com>
2023-11-06 12:54:46 +0200

    update package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pkg/**/*.templ"],
+  content: [
+    "./pkg/**/*.templ",
+    "./src/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+      },
+    ],
+  },
 }
 
