acc39105159bb2267ce0eb044c1244c9884fb5fc
dv1x3r <d.purvinsh@gmail.com>
2023-11-06 17:48:59 +0200

    todo split testing

--- cmd/main.go
+++ cmd/main.go
@@ -30,7 +30,27 @@ func main() {
 		return component.Render(c.Request().Context(), c.Response().Writer)
 	})
 
+	e.GET("/api/todo", func(c echo.Context) error {
+		var todos = []Todo{
+			{ID: 1, Name: "Test", Description: "Some text"},
+			{ID: 1, Name: "Test", Description: "Some text"},
+		}
+
+		for i := 0; i < 10000; i++ {
+			todos = append(todos, Todo{ID: int64(i), Name: "New", Description: "desc"})
+
+		}
+
+		return c.JSON(http.StatusOK, todos)
+	})
+
 	e.Logger.Fatal(e.Start("localhost:1323"))
+}
+
+type Todo struct {
+	ID          int64  `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
 
 func getUser(c echo.Context) error {
--- src/admin.js
+++ src/admin.js
@@ -1,18 +1,10 @@
-import 'w2ui/w2ui-2.0.min.css'
+import 'w2ui/w2ui-2.0.css'
 import { w2ui, w2layout, w2toolbar, w2sidebar, w2grid, query, w2utils } from 'w2ui/w2ui-2.0.es6'
+import { grid } from './admin/gridTodo'
 
 window.w2ui = w2ui
 
 let config = {
-  layout: {
-    name: 'layout',
-    box: '#page',
-    panels: [
-      { type: 'top', size: 40, style: 'border: 1px solid #efefef;' },
-      { type: 'left', size: 260, style: 'border: 1px solid #efefef;' },
-      { type: 'main', style: 'border: 1px solid #efefef;' },
-    ]
-  },
   toolbar: {
     name: 'toolbar',
     items: [
@@ -30,38 +22,19 @@ let config = {
 async function showTodos() {
   await layout.load('main', 'todos')
   grid.render('#grid')
-  grid.load('data')
+  grid.load('api/todo')
 }
 
-let grid = new w2grid({
-  name: 'grid',
-  header: 'List of Names',
-  reorderRows: false,
-  show: {
-    header: true,
-    footer: true,
-    toolbar: true,
-    lineNumbers: true
-  },
-  columns: [
-    { field: 'recid', text: 'ID', size: '30px' },
-    { field: 'fname', text: 'First Name', size: '30%' },
-    { field: 'lname', text: 'Last Name', size: '30%' },
-    { field: 'email', text: 'Email', size: '40%' },
-    { field: 'sdate', text: 'Start Date', size: '120px' }
-  ],
-  searches: [
-    { type: 'int', field: 'recid', label: 'ID' },
-    { type: 'text', field: 'fname', label: 'First Name' },
-    { type: 'text', field: 'lname', label: 'Last Name' },
-    { type: 'date', field: 'sdate', label: 'Start Date' }
-  ],
-  onExpand(event) {
-    query('#' + event.detail.box_id).html('<div style="padding: 10px; height: 100px">Expanded content</div>')
-  }
+
+let layout = new w2layout({
+  name: 'layout',
+  box: '#page',
+  panels: [
+    { type: 'top', size: 40, style: 'border: 1px solid #efefef;', html: new w2toolbar(config.toolbar) },
+    { type: 'left', size: 260, style: 'border: 1px solid #efefef;', html: "test"},
+    { type: 'main', style: 'border: 1px solid #efefef;' },
+  ]
 })
 
-let layout = new w2layout(config.layout)
-layout.html('top', new w2toolbar(config.toolbar))
 showTodos()
 
