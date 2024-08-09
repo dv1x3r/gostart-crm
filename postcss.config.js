f008818b04df3c1f834630f371807305f50f219f
dv1x3r <d.purvinsh@gmail.com>
2024-08-09 11:35:54 +0300

    add postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,19 @@
 module.exports = {
   map: false,
   plugins: [
-    require('@csstools/postcss-sass')({ sourceMap: false, includePaths: ['node_modules'] }),
+    require('@csstools/postcss-sass')({
+      sourceMap: false,
+      includePaths: ['node_modules'],
+    }),
+    require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
     require('cssnano'),
+    {
+      postcssPlugin: 'rename-files',
+      OnceExit(css, { result }) {
+        result.opts.to = result.opts.to.replace(/\.scss$/, '.css')
+      }
+    }
   ]
 }
--- web/js/client.js
+++ web/js/client.js
@@ -1,11 +1,7 @@
 import Htmx from 'htmx.org'
 window.htmx = Htmx
-window.htmx.config.includeIndicatorStyles = false
-window.htmx.config.scrollBehavior = 'smooth'
 
 import Alpine from 'alpinejs'
-import collapse from '@alpinejs/collapse'
 window.Alpine = Alpine
-Alpine.plugin(collapse)
 Alpine.start()
 
