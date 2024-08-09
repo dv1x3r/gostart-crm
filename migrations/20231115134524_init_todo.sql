a0525a996ef3e38f9f4b5494cd632eef2cbce8aa
dv1x3r <d.purvinsh@gmail.com>
2024-08-09 12:12:35 +0300

    update todo table

--- migrations/20231115134524_init_todo.sql
+++ migrations/20231115134524_init_todo.sql
@@ -1,10 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
 create table todo (
-    id integer primary key,
-    name text not null,
-    description text,
-    quantity integer
+    [id] integer primary key autoincrement,
+    [created_at] integer not null default (unixepoch()),
+    [updated_at] integer not null default (unixepoch()),
+    [deleted_at] integer,
+    [name] text not null,
+    [description] text,
+    [quantity] integer
 ) strict;
 -- +goose StatementEnd
 
