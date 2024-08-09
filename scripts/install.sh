4da415466e4e0c13a9c02b9518a6f421839ccf4f
dv1x3r <d.purvinsh@gmail.com>
2024-08-09 15:02:32 +0300

    add goose to the scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,5 +18,6 @@ if [[ $yn == "y" ]]; then
   go install github.com/go-delve/delve/cmd/dlv@latest
   go install github.com/cosmtrek/air@latest
   go install github.com/a-h/templ/cmd/templ@latest
+  go install github.com/pressly/goose/v3/cmd/goose@latest
 fi
 
