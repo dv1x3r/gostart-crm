210bb8d08b1341f6595ebe88e9a0edfdd5469b39
dv1x3r <d.purvinsh@gmail.com>
2024-08-13 13:54:02 +0300

    init field types model

--- internal/app/model/todo_model.go
+++ internal/app/model/todo_model.go
@@ -1,10 +1,32 @@
 package model
 
+import (
+	"encoding/json"
+)
+
 type TodoDTO struct {
 	ID          int64   `json:"id" db:"id"`
 	Name        string  `json:"name" db:"name"`
 	Description *string `json:"description,omitempty" db:"description"`
 	Quantity    *int64  `json:"quantity,omitempty" db:"quantity"`
+
+	FieldMap FieldMap `json:"-" db:"-"`
+}
+
+func (t *TodoDTO) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" || string(data) == `""` {
+		return nil
+	}
+
+	raw := make(FieldRaw)
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+
+	t.FieldMap = make(FieldMap)
+	t.ID = getValue[int64]("id", &raw, "ID", &t.FieldMap)
+
+	return nil
 }
 
 type TodoW2GridResponse = W2GridDataResponse[TodoDTO, any]
