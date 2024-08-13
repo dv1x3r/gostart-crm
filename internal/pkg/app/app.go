4b8fba43be2dd175780bf10b73d946c155f8d6e3
dv1x3r <d.purvinsh@gmail.com>
2024-08-14 01:29:42 +0300

    update pkg integration

--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -1,80 +1,156 @@
 package app
 
 import (
+	"context"
+	"net"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"gostart-crm/internal/app/endpoint"
 	"gostart-crm/internal/app/service"
 	"gostart-crm/internal/app/storage/sqlitedb"
+	"gostart-crm/internal/app/utils"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
 
-	_ "github.com/joho/godotenv/autoload"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type App struct {
-	echo          *echo.Echo
-	db            *sqlx.DB
-	indexEndpoint *endpoint.Index
-	adminEndpoint *endpoint.Admin
-	todoStorage   *sqlitedb.Todo
-	todoService   *service.Todo
+	config utils.Config
+	logger zerolog.Logger
+	echo   *echo.Echo
+	db     *sqlx.DB
 }
 
 func New() (*App, error) {
 	a := &App{}
 	a.echo = echo.New()
-	a.echo.Debug = true
+	a.config = utils.GetConfig()
+	a.logger = utils.GetLogger()
 
-	a.echo.Use(middleware.Logger())
-	a.echo.Use(middleware.Recover())
-
-	a.echo.Use(middleware.Gzip())
-	a.echo.Use(middleware.Secure())
-	a.echo.Use(middleware.CSRFWithConfig(
-		middleware.CSRFConfig{
-			CookiePath:     "/",
-			CookieSameSite: http.SameSiteLaxMode,
-		},
-	))
-
-	a.echo.Static("/", "./tmp/public")
-
-	dbDriver, dbString := os.Getenv("DB_DRIVER"), os.Getenv("DB_STRING")
-	if dbDriver == "sqlite3" {
-		dbString += "?_journal=WAL&_fk=1"
+	if a.config.DBDriver == "sqlite3" {
+		a.db = sqlx.MustConnect(a.config.DBDriver, a.config.DBString+"?_journal=WAL&_fk=1&_busy_timeout=10000")
+		a.db.SetMaxOpenConns(1)
+	} else if a.config.DBDriver == "sqlite" {
+		a.db = sqlx.MustConnect(a.config.DBDriver, a.config.DBString+"?_pragma=journal_mode(WAL)&_pragma=foreign_keys(1)&_pragma=busy_timeout(10000)")
+		a.db.SetMaxOpenConns(1)
+	} else {
+		a.db = sqlx.MustConnect(a.config.DBDriver, a.config.DBString)
 	}
-	a.db = sqlx.MustConnect(dbDriver, dbString)
 
-	a.todoStorage = sqlitedb.NewTodo(a.db)
-	a.todoService = service.NewTodo(a.todoStorage)
+	if a.config.Debug {
+		a.echo.Debug = true
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		zerolog.TimeFieldFormat = time.TimeOnly
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+		zerolog.TimeFieldFormat = time.RFC3339
+	}
 
-	a.indexEndpoint = endpoint.NewIndex(a.todoService)
-	a.echo.GET("/", a.indexEndpoint.GetRoot)
+	if a.config.StaticPath != "" {
+		a.echo.Static("/", a.config.StaticPath)
+	}
 
-	a.adminEndpoint = endpoint.NewAdmin(a.todoService)
-	a.adminEndpoint.Register(a.echo)
+	a.echo.Validator = utils.GetValidator()
 
-	a.echo.GET("/users/:id", func(c echo.Context) error {
-		// User ID from path `users/:id`
-		id := c.Param("id")
-		return c.String(http.StatusOK, id)
+	a.echo.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogRequestID:  true,
+		LogStatus:     true,
+		LogRemoteIP:   true,
+		LogLatency:    true,
+		LogHost:       true,
+		LogMethod:     true,
+		LogURI:        true,
+		LogError:      true,
+		HandleError:   true,
+		LogValuesFunc: utils.LogEchoRequestFunc,
+	}))
+
+	a.echo.Use(middleware.RecoverWithConfig(middleware.RecoverConfig{
+		LogErrorFunc: utils.LogEchoRecoverFunc,
+	}))
+
+	a.echo.Use(middleware.RequestID())
+	a.echo.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			requestID := c.Response().Header().Get(echo.HeaderXRequestID)
+			ctx := context.WithValue(c.Request().Context(), "RequestID", requestID)
+			c.SetRequest(c.Request().WithContext(ctx))
+			return next(c)
+		}
 	})
 
-	a.echo.GET("/show", func(c echo.Context) error {
-		// Get team and member from the query string
-		team := c.QueryParam("team")
-		member := c.QueryParam("member")
-		return c.String(http.StatusOK, "team:"+team+", member:"+member)
-	})
+	a.echo.Use(middleware.Secure())
+
+	if a.config.GZIP {
+		a.echo.Use(middleware.Gzip())
+	}
+
+	if a.config.CORS {
+		a.echo.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowOrigins: []string{"*"},
+			AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+		}))
+	}
+
+	// a.echo[0].Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+	// 	TokenLookup:    "header:X-CSRF-Token",
+	// 	CookiePath:     "/",
+	// 	CookieName:     "csrftoken",
+	// 	CookieMaxAge:   86400,
+	// 	CookieHTTPOnly: true,
+	// 	CookieSameSite: http.SameSiteStrictMode,
+	// }))
+
+	// a.echo[1].Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+	// 	TokenLookup:    "form:csrftoken",
+	// 	CookiePath:     "/",
+	// 	CookieName:     "csrftoken",
+	// 	CookieMaxAge:   86400,
+	// 	CookieHTTPOnly: true,
+	// 	CookieSameSite: http.SameSiteStrictMode,
+	// }))
+
+	todoStorage := sqlitedb.NewTodo(a.db)
+	todoService := service.NewTodo(todoStorage)
+
+	indexEndpoint := endpoint.NewIndex(todoService)
+	a.echo.GET("/", indexEndpoint.GetRoot)
+
+	adminEndpoint := endpoint.NewAdmin(todoService)
+	adminEndpoint.Register(a.echo)
 
 	return a, nil
 }
 
 func (a *App) MustRun() {
-	a.echo.Logger.Fatal(a.echo.Start("localhost:1323"))
+	if strings.HasSuffix(a.config.ServerAddress, ".sock") {
+		os.Remove(a.config.ServerAddress)
+		listener, err := net.Listen("unix", a.config.ServerAddress)
+		if err != nil {
+			a.logger.Fatal().Err(err).Msg("unable to create unix .sock listener")
+		}
+
+		if err := os.Chmod(a.config.ServerAddress, 0777); err != nil {
+			a.logger.Fatal().Err(err).Msg("unable set unix .sock permissions")
+		}
+
+		a.echo.Listener = listener
+
+		server := new(http.Server)
+		if err := a.echo.StartServer(server); err != nil {
+			a.logger.Fatal().Err(err).Msg("unable to create unix .sock listener")
+		}
+	} else {
+		err := a.echo.Start(a.config.ServerAddress)
+		if err != nil && err != http.ErrServerClosed {
+			a.logger.Fatal().Err(err).Msg("shutting down the server")
+		}
+	}
 }
